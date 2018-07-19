84351b23e57967f0838d286de9528cc7421080c4
simonsben <simons.ben0@gmail.com>
2018-07-18 23:22:03 -0400

    Some more progress

--- src/app/landing/Content.jsx
+++ src/app/landing/Content.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
 export const Content = props => {
+    const firstItem = 'Check out the top 10 beers right now!';
+    const secondItem = "Maybe consider picking up one that's on sale!";
+    const thirdItem = 'Or maybe consider checking out the vast list of brews!';
 
     return (
         <div className='landing_content'>
             <div className='content_item'>
-                
+                <PadContent text={firstItem}/>
+            </div>
+            <div className='content_item right'>
+                <PadContent text={secondItem}/>
             </div>
             <div className='content_item'>
-                
-            </div>
-            <div className='content_item'>
-                
+                <PadContent text={thirdItem}/>
             </div>
         </div>
     );
 }
+
+const PadContent = props => (
+    <div className='inner_content'>{props.text}</div>
+)
